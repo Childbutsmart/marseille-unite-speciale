d6a15766d6251a493c7d1b1b8a63fdc5e1b5ed55
Guillaume Martinez <guillaume.martinez.1804@gmail.com>
2024-03-25 09:24:40 +0100

    fin de l'intégration technique de la page de liste des criminels: affichage des cartes, recherches dynamique, fiche d'infos supp en fonction de la fiche sur laquelle on clique

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import List from './components/list/List.jsx';
+
 
 function App() {
-  
+    return (
+    <div>
+        <List/>
+    </div>
+    )
 }
 
-export default App
+export default App;
